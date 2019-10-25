52a2e7a04cbb4ca80b60b47fefb96d7d279b0684
Abada Samuel <abadasamuelosp@gmail.com>
2019-10-25 09:55:33 +0100

    ui

--- stutal/settings.py
+++ stutal/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
---
+++ user/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from user import views
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('auth', views.auth, name="auth"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
+    path('signup', views.sign_up_page, name="signup"),
+    path('create', views.sign_up, name="create"),
+    path('change_password', views.change_password, name="change_password"),
+]
